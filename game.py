5b3d570a5c702c8adfc4e68fef7f8d12edceb800
grochalapiotr <miami2042@gmail.com>
2022-04-01 16:43:51 +0200

    add colored keyboard

--- game.py
+++ game.py
@@ -7,7 +7,11 @@ with open("words.txt","r") as w:
         words.append(word[:-1])
 
 
+
 def play_game():
+    keyboard = """[q] [w] [e] [r] [t] [y] [u] [i] [o] [p]
+  [a] [s] [d] [f] [g] [h] [j] [k] [l]
+    [z] [x] [c] [v] [b] [n] [m]"""
     answer=random.choice(words)
     print(answer)           #testing word
     while True:
@@ -28,14 +32,22 @@ def play_game():
         if guess == answer:
             print(colored(guess, "green"))
             break
+
+
         for letter in range(len(guess)):
             if guess[letter] == answer[letter]:
                 print(colored(guess[letter], "green"), end = "")
+                keyboard = keyboard.replace(guess[letter], colored(guess[letter], "green"))
+            
             elif guess[letter] in answer:
                 print(colored(guess[letter], "yellow"), end = "")
+                if colored(guess[letter], "green") not in keyboard:
+                    keyboard = keyboard.replace(guess[letter], colored(guess[letter], "yellow"))
+            
             else:
                 print(colored(guess[letter], "red"), end = "")
-        print("")
+                keyboard = keyboard.replace(guess[letter], colored(guess[letter], "red"))
+        print("\n", keyboard,"\n")
     print("You won!!!")
 
 
