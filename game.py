9d70ae7158f2c809a94869faafa3b0f7b252b6b6
grochalapiotr <miami2042@gmail.com>
2022-04-03 21:35:19 +0200

    imporved yellow coloring

--- game.py
+++ game.py
@@ -7,16 +7,15 @@ with open("words.txt","r") as w:
         words.append(word[:-1])
 
 
-
 def play_game():
     keyboard = """[q] [w] [e] [r] [t] [y] [u] [i] [o] [p]
   [a] [s] [d] [f] [g] [h] [j] [k] [l]
     [z] [x] [c] [v] [b] [n] [m]"""
     answer=random.choice(words)
-    print(answer)           #testing word
+    #print(answer)           #for testing
     while True:
         guess = input("Enter 5 letter word: ")
-        
+
         while len(guess) > 5:
             print("Word is too long")
             guess = input("Enter 5 letter word: ")
@@ -33,20 +32,36 @@ def play_game():
             print(colored(guess, "green"))
             break
 
-
-        for letter in range(len(guess)):
+        guess = list(guess)
+        
+        
+        for letter in range(len(guess)):   
+            #green color   
             if guess[letter] == answer[letter]:
-                print(colored(guess[letter], "green"), end = "")
                 keyboard = keyboard.replace("["+guess[letter]+"]", "["+colored(guess[letter], "green")+"]")
+                keyboard = keyboard.replace("["+colored(guess[letter], "yellow")+"]", "["+colored(guess[letter], "green")+"]")
+                guess[letter] = colored(guess[letter], "green")
             
-            elif guess[letter] in answer:
-                print(colored(guess[letter], "yellow"), end = "")
-                if colored(guess[letter], "green") not in keyboard:
-                    keyboard = keyboard.replace("["+guess[letter]+"]", "["+colored(guess[letter], "yellow")+"]")
+            #yellow color
+            elif guess[letter] in answer:     
+                if guess.count(guess[letter]) > answer.count(guess[letter]):
+                    for letter_temp in range(letter+1, len(guess)):
+                        if guess[letter_temp] == answer[letter_temp]:
+                            break
+                        elif guess.count(colored(guess[letter], "yellow")) < answer.count(answer[letter]):
+                            guess[letter] = colored(guess[letter], "yellow")
+                elif colored(guess[letter], "yellow") not in guess:
+                    if colored(guess[letter], "green") not in keyboard:
+                        keyboard = keyboard.replace("["+guess[letter]+"]", "["+colored(guess[letter], "yellow")+"]")
+                    if guess.count(colored(guess[letter], "green")) != answer.count(guess[letter]):
+                        guess[letter] = colored(guess[letter], "yellow")
             
-            else:
-                print(colored(guess[letter], "red"), end = "")
+            #red color
+            else:       
                 keyboard = keyboard.replace("["+guess[letter]+"]", "["+colored(guess[letter], "red")+"]")
+                guess[letter] = colored(guess[letter], "red")
+
+        print(''.join(guess))
         print("\n", keyboard,"\n")
     print("You won!!!")
 
